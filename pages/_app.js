2963a16bddcf921659c7cb95fe8924f354a8d8fd
paul serafim <paul.serafim.3021@gmail.com>
2023-03-26 17:18:02 +1100

    code

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
+import { useEffect, useState } from 'react'
 
 function MyApp({ Component, pageProps }) {
+  const [showChild, setShowChild] = useState(false)
+
+  useEffect(() => {
+    setShowChild(true)
+  }, [])
+
+  if (!showChild) {
+    return null
+  }
+  
   return (
     <>
       <Navbar />
